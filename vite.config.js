8e8ab52e07690d74df879856f02f7f769b32d761
tortangChris <eliseriochrisashley@gmail.com>
2025-04-17 17:12:24 +0800

    test 5 serviceworker

--- vite.config.js
+++ vite.config.js
@@ -1,38 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { VitePWA } from "vite-plugin-pwa";
 
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    VitePWA({
-      registerType: "autoUpdate",
-      devOptions: {
-        enabled: true,
-      },
-      manifest: {
-        name: "EduAR Interactive Learning",
-        short_name: "EduAR",
-        description:
-          "EduAR is an interactive learning PWA that brings education to life through immersive AR experiences",
-        theme_color: "#ffffff",
-        background_color: "#ffffff",
-        display: "standalone",
-        scope: "/",
-        start_url: "/",
-        // icons: [
-        //   {
-        //     src: "pwa-192x192.png",
-        //     sizes: "192x192",
-        //     type: "image/png",
-        //   },
-        //   {
-        //     src: "pwa-512x512.png",
-        //     sizes: "512x512",
-        //     type: "image/png",
-        //   },
-        // ],
-      },
-    }),
-  ],
+  plugins: [react()],
 });
