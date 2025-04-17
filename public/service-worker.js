f30d460b6d0f66b6a712150af0f05b478ff4e179
tortangChris <eliseriochrisashley@gmail.com>
2025-04-17 15:03:35 +0800

    test 3 serviceworker

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,12 +1,28 @@
-self.addEventListener('install', (event) => {
-  console.log('[Service Worker] Installing...');
-  self.skipWaiting();
+self.addEventListener("install", (event) => {
+  console.log("Service Worker installing...");
+  event.waitUntil(
+    caches.open("eduAR-cache").then((cache) => {
+      return cache.addAll([
+        "/",
+        "/index.html",
+        "/favicon.ico",
+        "/manifest.json",
+        "/pwa-192x192.png",
+        "/pwa-512x512.png",
+      ]);
+    })
+  );
 });
 
-self.addEventListener('activate', (event) => {
-  console.log('[Service Worker] Activated!');
+self.addEventListener("activate", (event) => {
+  console.log("Service Worker activating...");
 });
 
-self.addEventListener('fetch', (event) => {
-  event.respondWith(fetch(event.request));
+self.addEventListener("fetch", (event) => {
+  console.log("Fetching:", event.request.url);
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
 });
