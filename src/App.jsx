cf72a80d780aa6bc670a0958b72d7b9627806dc2
tortangChris <eliseriochrisashley@gmail.com>
2025-04-17 14:49:20 +0800

    test serviceworker

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
 import React from "react";
+import InstallPrompt from "./InstallPrompt";
 
 const App = () => {
-  return <div>Sample Text</div>;
+  return (
+    <div>
+      Sample Text<h1>Welcome to EduAR</h1>
+      <InstallPrompt />
+    </div>
+  );
 };
 
 export default App;
